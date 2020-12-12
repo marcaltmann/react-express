4047fcafa89a6b78a97bee8ab121c89385c29f7d
Marc Altmann <marcaltmann@posteo.de>
2020-12-12 07:40:51 +0100

    Serve html page

--- src/routes/ssr.js
+++ src/routes/ssr.js
@@ -3,7 +3,21 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    res.status(201).send("Hello World");
+    const theHtml = `
+    <!DOCTYPE html>
+    <html>
+        <head><title>My First SSR</title></head>
+        <body>
+            <h1>My First Server Side Render</h1>
+            <div id="reactele">{{{reactele}}}</div>
+            <script src="/app.js" charset="utf-8"></script>
+            <script src="/vendor.js" charset="utf-8"></script>
+        </body>
+    </html>`;
+
+    res.setHeader('Cache-Control', 'max-age=5000');
+    res.status(200);
+    res.send(theHtml);
 });
 
 export default router;
