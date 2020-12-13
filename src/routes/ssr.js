c0c27571742cd857aeb96f8d8026f283400294db
Marc Altmann <marcaltmann@posteo.de>
2020-12-13 12:39:37 +0100

    Finish example

--- src/routes/ssr.js
+++ src/routes/ssr.js
@@ -1,4 +1,10 @@
 import express from "express";
+import prettier from 'prettier';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import hbs from 'handlebars';
+
+import App from '../components/app';
 
 const router = express.Router();
 
@@ -6,8 +12,8 @@ router.get("/", async (req, res) => {
     const theHtml = `
     <!DOCTYPE html>
     <html>
-        <head><title>My First SSR</title></head>
-        <body>
+    <head><title>My First SSR</title></head>
+    <body>
             <h1>My First Server Side Render</h1>
             <div id="reactele">{{{reactele}}}</div>
             <script src="/app.js" charset="utf-8"></script>
@@ -15,9 +21,14 @@ router.get("/", async (req, res) => {
         </body>
     </html>`;
 
+    const hbsTemplate = hbs.compile(theHtml);
+    const reactComp = renderToString(<App />);
+    console.log(reactComp);
+    const htmlToSend = hbsTemplate({ reactele: reactComp });
+
     res.setHeader('Cache-Control', 'max-age=5000');
     res.status(200);
-    res.send(theHtml);
+    res.send(prettier.format(htmlToSend, { parser: 'html' }));
 });
 
 export default router;
